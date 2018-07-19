21bdb86a559b7356ef630cf38dbda51555178dd7
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-07-19 23:54:53 +0200

    fix cache icon shadow

--- UDCT/RRAPP/worker.js
+++ UDCT/RRAPP/worker.js
@@ -18,8 +18,7 @@ self.addEventListener('install', function(event) {
           'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
           'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
           'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-          'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
-          'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow-2x.png'
+          'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png'
 
         ]);
       })
